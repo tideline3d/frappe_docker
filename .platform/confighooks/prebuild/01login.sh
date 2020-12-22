910b0b3b27a9b86b1acc97528df2497d7022bca5
jw59 <joshw59@gmail.com>
2020-12-22 14:20:52 -0500

    Dumb

--- .platform/confighooks/prebuild/01login.sh
+++ .platform/confighooks/prebuild/01login.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#USER=/opt/elasticbeanstalk/bin/get-config environment -k USER
-#PASSWD=/opt/elasticbeanstalk/bin/get-config environment -k PASSWD
+USER=/opt/elasticbeanstalk/bin/get-config environment -k USER
+PASSWD=/opt/elasticbeanstalk/bin/get-config environment -k PASSWD
 docker login -u $USER -p $PASSWD   

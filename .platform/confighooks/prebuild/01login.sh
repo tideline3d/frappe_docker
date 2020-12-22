2942b9649c328fc865f3338d541b40a88045e32b
jw59 <joshw59@gmail.com>
2020-12-22 14:04:20 -0500

    remove echo

--- .platform/confighooks/prebuild/01login.sh
+++ .platform/confighooks/prebuild/01login.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #USER=/opt/elasticbeanstalk/bin/get-config environment -k USER
 #PASSWD=/opt/elasticbeanstalk/bin/get-config environment -k PASSWD
-echo docker login -u $USER -p $PASSWD   
+docker login -u $USER -p $PASSWD   

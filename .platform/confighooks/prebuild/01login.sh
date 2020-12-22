c4dfebce828b9cd11ea35f8280ef5279516036e5
jw59 <joshw59@gmail.com>
2020-12-21 20:35:52 -0500

    Is this the exec format error?

--- .platform/confighooks/prebuild/01login.sh
+++ .platform/confighooks/prebuild/01login.sh
@@ -1,5 +1,5 @@
 ### example 01login.sh
 #!/bin/bash
-USER=/opt/elasticbeanstalk/bin/get-config environment -k USER
-PASSWD=/opt/elasticbeanstalk/bin/get-config environment -k PASSWD
+#USER=/opt/elasticbeanstalk/bin/get-config environment -k USER
+#PASSWD=/opt/elasticbeanstalk/bin/get-config environment -k PASSWD
 docker login -u $USER -p $PASSWD   
